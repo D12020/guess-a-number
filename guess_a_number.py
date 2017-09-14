08a7eae3dda84b62d660edbd3d14d4d8c2277975
Al-Nawasreh <salna9911@greenville.k12.sc.us>
2017-09-14 09:27:21 -0400

    added a guess limit

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,11 +1,18 @@
 import random
+#config
+low = 1
+high = 100
+limit = 10
 
-rand = random.randrange(1,100)
-print("I'm thinking of a number from 1 to 100.");
+# start game
+rand = random.randint(low,high)
+print("I'm thinking of a number from " + str(low) + " to " + str(high)+ ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+# play the game
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -13,7 +20,14 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
+    tries += 1
+   
+
+# tell player outcome
+if guess == rand:
+    print("You Win!!!!")
+else:
+    print ("You lose. I was thinking of the number" + str(rand) + ".")
+    
 print("Game over")
