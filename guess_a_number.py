886b5201ab6a9b7c178865e4013f8bd6879077ca
Al-Nawasreh <salna9911@greenville.k12.sc.us>
2017-09-18 09:30:35 -0400

    Lets you play the game again.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,15 +1,9 @@
 import random
 #config
 low = 1
-high = 100
-limit = 10
+high = 10
+limit = 4
 
-# start game
-rand = random.randint(low,high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high)+ ".");
-
-guess = -1
-tries = 0
 
 # helper functions
 def get_guess():
@@ -22,23 +16,45 @@ def get_guess():
         else:
             print("You must enter a number, silly.")
 
+def play_again():
+    while True:
+        decision = input (" Would you like to play again? (y/n) ")
+        if decision == "y" or decision == "yes" or decision == "yeah" or decision == "Yeah" or decision == "Yes" or decision == "Y" or decision == "si" or decision == "Si" or decision == "yee" or decision == "Yee":
+            return True
+        elif decision == "n" or decision == "no":
+            return False
 
-# play the game
-while guess != rand and tries < limit:
-    guess = get_guess()
- 
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
+        print(" I don't understand. Please gimme either 'y' or 'n'.")
 
-    tries += 1
-   
+again = True
 
-# tell player outcome
-if guess == rand:
-    print("You Win!!!!")
-else:
-    print ("You lose. I was thinking of the number" + str(rand) + ".")
+while again:
+    # start game
+    rand = random.randint(low,high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high)+ ".");
+
+    guess = -1
+    tries = 0
+
+
+    # play the game
+    while guess != rand and tries < limit:
+        guess = get_guess()
+     
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
+
+        tries += 1
+
+
+    # tell player outcome
+    if guess == rand:
+        print("You Win!!!!")
+    else:
+        print ("You lose. I was thinking of the number " + str(rand) + ".")
+        
+    again = play_again()
     
 print("Game over")
